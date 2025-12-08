34ed093dd4f9c4d6c15b54212e099c1bc96ed837
Dbol07 <blackregulus.007subra@gmail.com>
2025-12-08 09:50:37 -0500

    Update AppLayout.tsx

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
+import BottomNav from "@/components/BottomNav";
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen w-full bg-[#f2ebd7] flex flex-col">
-      
-      {/* MAIN CONTENT AREA */}
-      <div className="flex-1">
-        <Outlet />   {/* ⭐ THIS IS WHAT WAS MISSING ⭐ */}
-      </div>
+
+      {/* MAIN CONTENT */}
+      <main className="flex-1 w-full mx-auto max-w-5xl px-4 pb-24">
+        <Outlet />
+      </main>
 
       {/* BOTTOM NAVIGATION */}
-      <div className="w-full mt-4">
-        {/* keep your bottom nav here */}
-      </div>
+      <footer className="w-full fixed bottom-0 left-0 bg-white shadow-lg z-50">
+        <BottomNav />
+      </footer>
     </div>
   );
 }
